5cacf0299e9b115f749a3c20ee5bfface06ad07c
JuliaGrisales <jgrisales@gmail.com>
2020-07-06 17:41:07 +0200

    Destructuring

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello World React")
+console.log("Destructuring")
+
+const persona = {
+  name: "Tony",
+  age: 45,
+  password: "Ironman"
+}
+
+console.log(persona.name)
+console.log(persona.age)
+console.log(persona.password)
+
+const { name, age: age2, password } = persona
+console.log(name, age2, password)
+
+const returnPersona = ({ name, age, range = "Captain" }) => {
+  console.log(name, age, range)
+}
+
+returnPersona(persona)
+
+const useContext = ({ password, name, age, range }) => {
+  return {
+    keyName: name,
+    years: age
+  }
+}
+
+const { keyName, years } = useContext(persona)
+console.log(keyName, years)
