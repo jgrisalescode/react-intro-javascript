cd626a4556aeee9c930f54ac3daa1f3750834c75
JuliaGrisales <jgrisales@gmail.com>
2020-07-08 16:13:36 +0200

    Async Await

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 // https://developers.giphy.com/dashboard/
-const apiKey = "nZ82xzf9w70zG26q8IHBafOoiIx6OC2k"
-// Using FetchAPI
-const request = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
-request
-  .then(resp => resp.json())
-  .then(({ data }) => {
-    console.log(data.images.original.url)
+
+const getImage = async () => {
+  try {
+    const apiKey = "nZ82xzf9w70zG26q8IHBafOoiIx6OC2k"
+    const response = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+    // Await before fetch converts response variable in Sync action
+    const { data } = await response.json()
+    const { url } = data.images.original
+    console.log(url)
     const img = document.createElement("img")
-    img.src = data.images.original.url
+    img.src = url
     document.body.append(img)
-  })
-  .catch(console.warn)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getImage()
