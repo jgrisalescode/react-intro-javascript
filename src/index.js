0b16e16f5ccf16b195c77a347a13c4cb904c98b8
JuliaGrisales <jgrisales@gmail.com>
2020-07-08 16:23:43 +0200

    Ternary Conditional

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-// https://developers.giphy.com/dashboard/
+const active = true
 
-const getImage = async () => {
-  try {
-    const apiKey = "nZ82xzf9w70zG26q8IHBafOoiIx6OC2k"
-    const response = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
-    // Await before fetch converts response variable in Sync action
-    const { data } = await response.json()
-    const { url } = data.images.original
-    console.log(url)
-    const img = document.createElement("img")
-    img.src = url
-    document.body.append(img)
-  } catch (error) {
-    console.error(error)
-  }
+let message = ""
+
+if (active) {
+  message = "Acrtive"
+} else {
+  message = "Inactive"
 }
 
-getImage()
+console.log(message)
+
+const message2 = active ? "Active" : "Inactive"
+console.log(message2)
+
+const message3 = active && "Active" // Without else
+console.log(message3)
